6a7623572b257ed1547ca7b8ace5fe57fd61f84a
Huzaifa Ghouri <120574411+HuzaifaGhouri08@users.noreply.github.com>
2023-03-04 18:18:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-class TodoList {
-  constructor() {
-    this.addButton = document.getElementById('btn');
-    this.inputField = document.getElementById('input');
-    this.tasks = document.getElementById('box');
-    this.removeButton = document.getElementById('rem');
+ class TodoList {
+    constructor() {
+      this.tasks = [];
+    }
 
-    this.addButton.addEventListener('click', () => this.addTask());
-    this.inputField.addEventListener('input', () => this.showButton());
-    this.tasks.addEventListener('click', (e) => this.removeSpecificTask(e));
-    this.removeButton.addEventListener('click', () => this.removeAllTasks());
-  }
+    addTask(task) {
+      this.tasks.push(task);
+    }
 
-  showButton() {
-    let val = this.inputField.value.trim();
-    if (val === '') {
-      this.addButton.style.display = 'none';
-    } else {
-      this.addButton.style.display = 'block';
+    removeTaskByIndex(index) {
+      this.tasks.splice(index, 1);
+    }
+
+    removeAllTasks() {
+      this.tasks = [];
     }
   }
 
-  addTask() {
-    document.getElementById('section2').style.animation = '1s slidein';
-    let input = this.inputField.value;
+  let MyTodoList = new TodoList();
+
+  let addButton = document.getElementById('btn');
+  let inputField = document.getElementById('input');
+  let tasks = document.getElementById('box');
+  let removeButton = document.getElementById('rem');
+
+  addButton.addEventListener('click', () => {
+    let input = inputField.value;
     if (input === '') {
-      alert('Please enter your task.');
+      alert('Please Enter Your Task.');
     } else {
       let li = document.createElement('li');
       li.textContent = input;
-      this.tasks.appendChild(li);
+      tasks.appendChild(li);
       document.querySelector('li').style.animation = '1s text';
 
       let a = document.createElement('a');
@@ -36,29 +38,47 @@ class TodoList {
       a.href = '#';
       a.className = 'remove';
       li.appendChild(a);
-      this.removeButton.style.display = 'block';
-    }
-    this.inputField.value = '';
-    this.addButton.style.display = 'none';
-  }
+      removeButton.style.display = 'block';
 
-  removeSpecificTask(e) {
+      MyTodoList.addTask(input);
+    }
+    inputField.value = '';
+    addButton.style.display = 'none';
+  });
+
+  inputField.addEventListener('input', () => {
+    let val = inputField.value.trim();
+    if (val === '') {
+      addButton.style.display = 'none';
+    } else {
+      addButton.style.display = 'block';
+    }
+  });
+
+  tasks.addEventListener('click', (e) => {
     let li = e.target.parentNode;
     if (li.nodeName === 'LI') {
-      this.tasks.removeChild(li);
-      if (this.tasks.childNodes.length === 0) {
-        this.removeButton.style.display = 'none';
+      let index = Array.from(tasks.children).indexOf(li);
+      MyTodoList.removeTaskByIndex(index);
+      tasks.removeChild(li);
+      if (tasks.childNodes.length === 0) {
+        removeButton.style.display = 'none';
       }
     }
-  }
+  });
 
-  removeAllTasks() {
-    document.getElementById('section2').style.animation = '2s fadeout';
-    let animation = setTimeout(() => {
-      this.tasks.innerHTML = '';
-      this.removeButton.style.display = 'none';
-    }, 2000);
-  }
-}
+  removeButton.addEventListener('click', () => {
+    let section2 = document.getElementById('section2');
+    section2.style.animation = '2s fadeout';
 
-let MyTodoList = new TodoList();
+    section2.addEventListener(
+      'animationend',
+      () => {
+        section2.style.animation = '';
+        tasks.innerHTML = '';
+        removeButton.style.display = 'none';
+        MyTodoList.removeAllTasks();
+      },
+      { once: true }
+    );
+  });
